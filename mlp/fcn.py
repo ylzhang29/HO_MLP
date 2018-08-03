17a379350702c53cecae15898c593f5e7f94451c
Amir Ghamarian <amir@machine2learn.nl>
2018-08-03 17:05:44 +0200

    Adds multi-label

--- mlp/fcn.py
+++ mlp/fcn.py
@@ -46,6 +46,7 @@ class FCN:
                 self.reg_ground_truth_slicer = config.get_as_slice(task, "ground_truth_column")
             elif config.get(task, 'type') == 'classification':
                 self.ground_truth_slicer = config.get_as_slice(task, "ground_truth_column")
+                self.multilabel = bool(config.get(task, 'multilabel'))
 
     def variable_summaries(self, var, name, task_tag):
         """Attach a lot of summaries to a Tensor."""
@@ -77,8 +78,44 @@ class FCN:
         last_hidden_layer = previous_out
         return last_hidden_layer
 
-    def add_classification_output_layer(self, last_hidden_layer, gt_labels, num_classes, corpus_tag, task_tag,
+    def add_multiple_classification_output_layer(self, last_hidden_layer, gt_labels, num_classes, corpus_tag, task_tag,
                                         loss_weight=1):
+        with tf.variable_scope("output_layer_%s" % task_tag) as layer_scope:
+            output_dim = gt_labels.shape[1].value
+            last_out = fully_connected(last_hidden_layer, output_dim, activation_fn=tf.identity,
+                                       weights_regularizer=self.l1_l2_regularizer,
+                                       scope=layer_scope)
+            self.predictions = tf.map_fn(tf.nn.sigmoid, last_out)
+
+        with tf.name_scope("%s_%s_stats" % (corpus_tag, task_tag)):
+            loss = loss_weight * tf.reduce_mean(
+                tf.nn.sigmoid_cross_entropy_with_logits(labels=gt_labels, logits=last_out))
+
+            self.variable_summaries(loss, "loss", task_tag)
+
+            tf.add_to_collection(tf.GraphKeys.LOSSES, loss)
+
+            str_accu, _ = streaming_accuracy(tf.map_fn(tf.rint, last_out), gt_labels, name="stracc_%s" % corpus_tag,
+                                             updates_collections=tf.GraphKeys.UPDATE_OPS)
+
+            str_accu = 100 * str_accu
+            # utils.variable_summaries(str_accu, "streaming_accuracy", corpus_tag)
+            self.variable_summaries(str_accu, "streaming_accuracy", task_tag)
+
+            updates_op = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+            self.streaming_accu_op = control_flow_ops.with_dependencies(updates_op, str_accu)
+
+            correct_prediction = tf.equal(tf.map_fn(tf.rint, last_out), gt_labels)
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) * 100
+
+            auc = tf.metrics.auc(gt_labels, self.predictions)
+            # utils.variable_summaries(accuracy, "accuracy", corpus_tag)
+            self.variable_summaries(accuracy, "accuracy", task_tag)
+            self.accuracy = accuracy
+            self.auc = auc
+
+    def add_classification_output_layer(self, last_hidden_layer, gt_labels, num_classes, corpus_tag, task_tag,
+                                           loss_weight=1):
         with tf.variable_scope("output_layer_%s" % task_tag) as layer_scope:
             last_out = fully_connected(last_hidden_layer, num_classes, activation_fn=tf.identity,
                                        weights_regularizer=self.l1_l2_regularizer,
@@ -119,7 +156,8 @@ class FCN:
 
     def add_linear_output_layer(self, last_hidden_layer, ground_truth, corpus_tag, task_tag, loss_weight=1):
         with tf.variable_scope("output_layer_%s" % task_tag) as layer_scope:
-            last_out = fully_connected(last_hidden_layer, 1, activation_fn=tf.identity,
+            output_dim = ground_truth.shape[1].value
+            last_out = fully_connected(last_hidden_layer, output_dim, activation_fn=tf.identity,
                                        weights_regularizer=self.l1_l2_regularizer,
                                        scope=layer_scope)
             last_out = tf.squeeze(last_out)
@@ -160,8 +198,12 @@ class FCN:
             elif task_config["type"] == "classification":
                 task_name += "_classf"
                 num_classes = int(task_config["num_classes"])
-                ground_truth = tf.to_int64(ground_truth)
-                self.add_classification_output_layer(hidden_output, ground_truth, num_classes, corpus_tag,
+
+                if self.multilabel == True:
+                    self.add_multiple_classification_output_layer(hidden_output, ground_truth, num_classes, corpus_tag,
+                                                     task_name)
+                else:
+                    self.add_classification_output_layer(hidden_output, ground_truth, num_classes, corpus_tag,
                                                      task_name)
             else:
                 assert False
@@ -173,8 +215,32 @@ class FCN:
         # Fix the shapes
         input_features = tf.placeholder(dtype=tf.float32, shape=(None, column_size))
 
-        input_labels = tf.placeholder(dtype=tf.int64, shape=(None,))
-        reg_input_labels = tf.placeholder(dtype=tf.float32, shape=(None,))
+        input_labels = None
+        reg_input_labels = None
+
+        if self.ground_truth_slicer:
+            labels_columns = list(range(self.total_column_size))[self.ground_truth_slicer]
+            if type(labels_columns) is list:
+                num_labels = len(labels_columns)
+            else:
+                num_labels = 1
+            if num_labels > 1:
+                input_labels = tf.placeholder(dtype=tf.float32, shape=(None, num_labels))
+
+        if input_labels is None:
+            input_labels = tf.placeholder(dtype=tf.int64, shape=(None,))
+
+        if self.reg_ground_truth_slicer:
+            labels_columns = list(range(self.total_column_size))[self.reg_ground_truth_slicer]
+            if type(labels_columns) is list:
+                num_labels = len(labels_columns)
+            else:
+                num_labels = 1
+            if num_labels > 1:
+                reg_input_labels = tf.placeholder(dtype=tf.float32, shape=(None, num_labels))
+
+        if reg_input_labels is None:
+            reg_input_labels = tf.placeholder(dtype=tf.float32, shape=(None,))
 
         return input_features, input_labels, reg_input_labels
 
